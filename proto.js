5fe7b31cf2685671a623a7d8db9e5a383237037f
Ivan <ivan_i1@hotmail.com>
2022-08-04 00:59:14 -0500

    Add methods for probability chunks and proto function for combat

--- proto.js
+++ proto.js
@@ -59,12 +59,44 @@ var createDistribution = function createDistribution(config) {
     return [distributionArray, percentageDistributionArray];
 }
 
+//core methods are exclusive
+//values used in core methods are adjusted to array key not dice values
+//e.i. for 5d6 value 0 means 5, value 6 means 11
+var moreThan = function moreThan(distributionArray, value) {
+    return distributionArray.reduce(function(p, c, i) {
+        if (i > value) {
+            return p + c;
+        }
+        return p;
+    }, 0)
+}
+var lessThan = function lessThan(distributionArray, value) {
+    return distributionArray.reduce(function(p, c, i) {
+        if (i < value) {
+            return p + c;
+        }
+        return p;
+    }, 0)
+}
+var between = function between(distributionArray, a, b) {
+    var full = lessThan(distributionArray, b);
+    var diff = lessThan(distributionArray, a);
+    return full - diff;
+}
+
+var protoCombat = function protoCombat(dist, atk, def) {
+    var needed = def - atk - 1 - config.dieNumber;
+    console.log(def, atk, config.dieNumber);
+    return between(dist, needed, dist.length);
+}
+
 var config = {
     numberOfSimulations: 200000,
-    dieNumber: 5,
-    dieSides: 4
+    dieNumber: 2,
+    dieSides: 6
 }
 config.numberOfResults = config.dieNumber * (config.dieSides - 1);
 var res = simulateRolls(config)
 config.results = res[0];
 var distRes = createDistribution(config)
+between(distRes[1], 0, 15)
